2611d37220b86c2bb66364e8fcdf75a17291c2b2
xiao-8810 <56467005+xiao-8810@users.noreply.github.com>
2019-10-16 23:35:05 +0800

    Add files via upload

--- work6/tarray.js
+++ work6/tarray.js
@@ -1,10 +1,17 @@
+//提取数组中不等于2的数字
+//创建数组
 var arr = [1,2,3,4,2,5,6,2,7,2];
+//显示数组
 document.getElementById('array').innerHTML = arr.toString()
+//index表示新数组newarr的下标，默认为0
 var index = 0,newArr = [];
+//遍历数组
 for(var i in arr){
-    if(arr[i] != 2){
-    newArr[index]= arr[i];
+    //数组元素不等于2，则将其保存到newarr中
+    if(arr[i] !=2){
+    newArr[index] = arr[i];
     ++index;
     }
 }
-document,getElementById('transArray').innerHTML = newArr.toString()
+//显示提取后的数组
+document.getElementById('transArray').innerHTML = newArr.toString()

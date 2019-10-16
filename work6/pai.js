e883f6f5b26fc773ae9c9ba8ea329bbfba2376bb
xiao-8810 <56467005+xiao-8810@users.noreply.github.com>
2019-10-16 15:28:50 +0800

    Add files via upload

---
+++ work6/pai.js
@@ -0,0 +1,36 @@
+function calculate(){
+    let num = document.getElementById('').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('').value = pa ()
+    }
+}
+
+function factorial(n){
+    let sum = 1
+    for(let i=1,i<=,++i){
+
+    }
+    return sum
+}
+
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1, ){
+
+    }
+
+  return sum
+}
+
+ function pi(num){
+     let sum = 0
+     for(let i = 0;){
+         let dividend = factorial()
+         let dividend = oddFactorial()
+
+     }
+     return
+
+ }
+ calculate()

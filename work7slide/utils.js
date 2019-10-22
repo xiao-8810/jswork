b8758438c47c7689c7181d0f94b7b2921f6671a8
xiao-8810 <56467005+xiao-8810@users.noreply.github.com>
2019-10-22 22:18:16 +0800

    Add files via upload

--- work7slide/utils.js
+++ work7slide/utils.js
@@ -46,7 +46,35 @@ showElement.innerHTML = str
 if (pos[0] != -1)
     setTimeout("showLog(logValues,showDiv)", 500)   
 }
-function inserSort(str){
-    //请自行完成插入排序用于动画演示的代码，以完成挑战任务
-
+function insertSort(str) {
+    let arr  = str.split(',')
+    let sortLog = []
+    for (let i = 1; i < arr.length; i++){
+        for(let j = i; j>0; j--) {
+            sortLog.push([arr.concat(), [j,j + 1]])
+            sortLog.push([arr.concat(), [j,j + 1]])
+            sortLog.push([arr.concat(), [j,j + 1]])
+            if (arr[j - 1] > arr[j]) {
+                [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
+                sortLog.push([arr.concat(), [j,j + 1]])
+            }
+        }
+    }
+    sortLog.push([arr.concat(), [-1,-1]])
+    return sortLog
 }
+function showLog(logValues, showElement) {
+    let str = ''
+    let {
+        done,
+        value: [row, pos]
+    } = logValues.next()
+for (const key in row) {
+    let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' : ''
+    str += '<span style="font-size:' + row[key] * 20 + 'px;' + color + '">' + row[key] + '</span>'
+    
+}
+showElement.innerHTML = str
+    if (pos[0] != -1)
+    setTimeout("showLog(logValues,showDiv)",500)
+    }

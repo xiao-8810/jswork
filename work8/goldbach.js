4cbebcd0d197ce7aaaef181d848431db72f8be35
xiao-8810 <3181521861@qq.com>
2019-10-24 12:46:36 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,1 +1,27 @@
-HTMLDetailsElement
+function bach(i){
+    var sum = 0;
+    
+    for(var d=1;d<=i;d++){
+        if(i% d == 0){
+            sum++;
+        }
+    }
+    if(sum==2){
+        return true;
+    }else{
+        return false;
+    }
+    }
+    function goldbach(odd){
+     
+        let str="";
+        odd = document.getElementById("odd").value
+        odd = parseInt(odd);
+     for(var x=2;x<odd;x++){
+        var y =odd-x;
+        if(bach(x)&&bach(y)&&x<=y){
+       str +=(odd+"可以拆分为两个质数" +x +"与" +y +"的和"+ '<br>')
+        }
+    }
+    document.getElementById('goldbach').innerHTML = str;
+    }
